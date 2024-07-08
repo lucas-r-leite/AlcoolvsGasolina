bbbc1a31822a7c56b7f7b48b4c246fd76b5a84f6
lucas <lucasgit.breeding934@passinbox.com>
2024-07-08 10:14:03 -0300

    First Commit

---
+++ app/src/main/java/com/example/alcoolVsGasolina/MainActivity.kt
@@ -0,0 +1,114 @@
+package com.example.alcoolVsGasolina
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.alcoolVsGasolina.ui.theme.AlcoolVsGasolinaTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AlcoolVsGasolinaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    AlcoolVsGasolina()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AlcoolVsGasolina(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 32.dp, start = 16.dp, end = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Álcool ou Gasolina ?",
+            style = TextStyle(
+                color = Color.LightGray,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        var valorGasolina = remember {
+            mutableStateOf("")
+        }
+
+        var valorAlcool = remember {
+            mutableStateOf("")
+        }
+
+
+        Spacer(modifier = Modifier.size(16.dp))
+        TextField(
+            value = valorGasolina.value,
+            onValueChange = {newValue ->
+                valorGasolina.value = newValue
+            },
+            label = {
+                Text(text = "Digite o valor da gasolina" )
+            }
+        )
+
+        Spacer(modifier = Modifier.size(16.dp))
+        TextField(
+            value = valorAlcool.value,
+            onValueChange = {newValue ->
+                valorAlcool.value = newValue
+            },
+            label = {
+                Text(text = "Digite o valor do álcool" )
+            }
+        )
+
+        if (valorGasolina.value != "" && valorAlcool.value != "") {
+            val result = valorAlcool.value.toDouble() / valorGasolina.value.toDouble() > 0.7
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(text = "Resultado: $result")
+
+        }
+
+    }
+
+
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlcoolVsGasolinaPreview() {
+    AlcoolVsGasolinaTheme {
+        AlcoolVsGasolina()
+    }
+}
